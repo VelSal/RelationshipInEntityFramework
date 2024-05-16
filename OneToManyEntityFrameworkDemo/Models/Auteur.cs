4cc961342a748f01bcedabc2c6c7f7c6d2c20e5b
VelSal <v.salvatore@live.be>
2024-05-16 09:47:03 +0200

    Ajoutez des fichiers projet.

---
+++ OneToManyEntityFrameworkDemo/Models/Auteur.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OneToManyEntityFrameworkDemo.Models
+{
+    public class Auteur
+    {
+        [Key]
+        public int AuteurId { get; set; }
+        public string Naam { get; set; }
+        public ICollection<Boek> Boeken { get; set; }
+
+    }
+}
---
+++ OneToManyEntityFrameworkDemo/Models/Boek.cs
@@ -0,0 +1,10 @@
+﻿namespace OneToManyEntityFrameworkDemo.Models
+{
+    public class Boek
+    {
+        public int BoekId { get; set; }
+        public string Title { get; set; }
+        public int AuteurId { get; set; }
+        public Auteur Auteur { get; set; }
+    }
+}
