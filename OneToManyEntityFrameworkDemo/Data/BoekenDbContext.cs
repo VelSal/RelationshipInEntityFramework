6ac47d6a8749c5d2315d65f6c4619dd8124a234d
VelSal <v.salvatore@live.be>
2024-05-16 10:19:14 +0200

    ok

---
+++ OneToManyEntityFrameworkDemo/Data/BoekenDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OneToManyEntityFrameworkDemo.Models;
+
+namespace OneToManyEntityFrameworkDemo.Data
+{
+    public class BoekenDbContext : DbContext 
+    {
+        public BoekenDbContext(DbContextOptions<BoekenDbContext> options) : base(options) { }
+        public DbSet<Auteur> Auteurs { get; set; }
+        public DbSet<Boek> Boeks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //Configure Auteur entity (or table)
+            modelBuilder.Entity<Auteur>().HasMany(a => a.Boeken).WithOne(b => b.Auteur).HasForeignKey(b => b.AuteurId);
+        }
+    }
+}
