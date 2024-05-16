e6a23ca50020f7396234d56923b125b5c4803dad
VelSal <v.salvatore@live.be>
2024-05-16 11:46:04 +0200

    ok

--- OneToManyEntityFrameworkDemo/Data/BoekenDbContext.cs
+++ OneToManyEntityFrameworkDemo/Data/BoekenDbContext.cs
@@ -12,31 +12,7 @@ namespace OneToManyEntityFrameworkDemo.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-
-            //Configure Auteur entity (or table)
-            modelBuilder.Entity<Auteur>()
-                .HasMany(a => a.Boeken)
-                .WithOne(b => b.Auteur)
-                .HasForeignKey(b => b.AuteurId)
-                .OnDelete(DeleteBehavior.NoAction);
-
-            //Seed data
-            modelBuilder.Entity<Auteur>().HasData(
-                new Auteur { AuteurId = 1, Naam = "I. Asimov" },
-                new Auteur { AuteurId = 2, Naam = "F. Dostoievski" },
-                new Auteur { AuteurId = 3, Naam = "S. King" },
-                new Auteur { AuteurId = 4, Naam = "J.R.R. Tolkien" }
-            );
-            modelBuilder.Entity<Boek>().HasData(
-                new Boek { BoekId = 1, Title = "Foundation", AuteurId = 1 },
-                new Boek { BoekId = 2, Title = "Crimes et Châtiments", AuteurId = 2 },
-                new Boek { BoekId = 3, Title = "Carrie", AuteurId = 3 },
-                new Boek { BoekId = 4, Title = "The Lord of the Ring", AuteurId = 4 },
-                new Boek { BoekId = 5, Title = "Robots", AuteurId = 1 },
-                new Boek { BoekId = 6, Title = "Les frères Karkarov", AuteurId = 2 },
-                new Boek { BoekId = 7, Title = "Les Régulateurs", AuteurId = 3 },
-                new Boek { BoekId = 8, Title = "The Hobbits", AuteurId = 4 }
-            );
+            SeedData.AddRecords(modelBuilder);
         }
     }
 }
---
+++ OneToManyEntityFrameworkDemo/Migrations/20240516093543_Added book.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OneToManyEntityFrameworkDemo.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addedbook : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Boeks",
+                columns: new[] { "BoekId", "AuteurId", "Title" },
+                values: new object[] { 9, 4, "Bombadil" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Boeks",
+                keyColumn: "BoekId",
+                keyValue: 9);
+        }
+    }
+}
